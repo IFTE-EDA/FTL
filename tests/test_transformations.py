3a1ce6f4a599558b2095519f83c5c899af5acbea
nico-arnold <nico.arnold@tu-dresden.de>
2023-09-26 15:35:29 +0200

    Implement transformation testing via pytest

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -21,43 +21,22 @@ class ParentDummy:
         pass
 
 
-def compare_to_file(points, filename) -> bool:
-    with open(os.path.join(os.getcwd(), filename), "r") as f:
-        if f.read() == points.__repr__():
-            return True
-        else:
-            return False
+class Test_Transformations:
+    def setup_class(self):
+        self.parent = ParentDummy()
+        self.mesh = v.load(os.path.abspath("data/Teststrip.stl")).clean()
 
+    def compare_to_file(self, points, filename) -> bool:
+        with open(os.path.join(os.getcwd(), filename), "r") as f:
+            if f.read() == points.__repr__():
+                return True
+            else:
+                return False
 
-def process_transformation(
-    tr: FTL.Transformation, res: FTL.Transformation, points: np.ndarray
-) -> bool:
-    for pid, pt in enumerate(points):
-        if tr.isInScope(pt):
-            vec = np.array([pt[0], pt[1], pt[2], 1])
-            vec = np.dot(tr.getMatrixAt(pt), vec)
-            points[pid][0] = vec[0]
-            points[pid][1] = vec[1]
-            points[pid][2] = vec[2]
-        elif tr.addResidual and res.isInScope(pt):
-            vec = np.array([pt[0], pt[1], pt[2], 1])
-            vec = np.dot(res.getMatrixAt(pt), vec)
-            points[pid][0] = vec[0]
-            points[pid][1] = vec[1]
-            points[pid][2] = vec[2]
-    return points
-
-
-def process_all(input, filename):
-    # plt = v.Plotter(N=len(input))  # .user_mode(v.interactor_modes.MousePan())
-    parent = ParentDummy()
-    mesh = v.load(os.path.abspath(filename)).clean()
-
-    print("Testing...")
-    for i, tr in enumerate(input):
-        mesh2 = mesh.clone()
+    def process_transformation(self, tr: FTL.Transformation) -> bool:
+        mesh2 = self.mesh.clone()
         points = mesh2.points()
-        tr.parent = parent
+        tr.parent = self.parent
         tr.name = tr.__class__.__name__
         if tr.addResidual:
             res = tr.getResidualTransformation()
@@ -66,59 +45,81 @@ def process_all(input, filename):
 
         if tr.transformWholeMesh:
             # Transformation implemented a method to  the whole transformation on its own
-            points = tr.transformMesh(mesh.clone()).points()
+            points = tr.transformMesh(self.mesh.clone()).points()
         else:
-            process_transformation(tr, res, points)
+            for pid, pt in enumerate(points):
+                if tr.isInScope(pt):
+                    vec = np.array([pt[0], pt[1], pt[2], 1])
+                    vec = np.dot(tr.getMatrixAt(pt), vec)
+                    points[pid][0] = vec[0]
+                    points[pid][1] = vec[1]
+                    points[pid][2] = vec[2]
+                elif tr.addResidual and res.isInScope(pt):
+                    vec = np.array([pt[0], pt[1], pt[2], 1])
+                    vec = np.dot(res.getMatrixAt(pt), vec)
+                    points[pid][0] = vec[0]
+                    points[pid][1] = vec[1]
+                    points[pid][2] = vec[2]
+            return points
         print(
             "-> Comparing {}: {}".format(
-                tr.name, compare_to_file(points, filename)
+                tr.name, self.compare_to_file(points, filename)
             )
         )
 
         mesh2.points(points)
-        # plt.at(i).show(mesh2)
-    # plt.interactive().close()
 
+    def process_all(self, input, filename):
+        self.test_DirBend()
+        self.test_LinearTransformation()
+        self.test_Spiral()
+        self.test_ZBend()
 
-input = []
+    def test_ZBend(self):
+        tr_zbend = FTL.ZBend(-20, 20, -10, 10, 90, FTL.DIR.POSX)
+        self.process_transformation(tr_zbend)
 
-json_db = {
-    "points": [
-        {"x": -20, "y": 10},
-        {"x": 0, "y": -10},
-        {"x": 60, "y": -10},
-        {"x": 60, "y": 10},
-    ],
-    "angle": 90,
-}
-json_sp = {
-    "points": [
-        {"x": -40, "y": 10},
-        {"x": -20, "y": -10},
-    ],
-    "length": 70,
-    "dir": "POSX",
-    "turns": 1,
-}
-angle = -pi / 6
-mat = [
-    [cos(angle), 0, sin(angle), 0],
-    [0, 1, 0, 0],
-    [-sin(angle), 0, cos(angle), 0],
-]
-bounds = box(-60, -10, 60, 10)
-tr_zbend = FTL.ZBend(-20, 20, -10, 10, 90, FTL.DIR.POSX)
-tr_dirbend = FTL.DirBend(json_db)
-tr_lin = FTL.LinearTransformation(
-    mat,
-    bounds,
-)
-tr_spiral = FTL.Spiral(json_sp)
-# res = tr.getResidualTransformation()
+    def test_DirBend(self):
+        json_db = {
+            "points": [
+                {"x": -20, "y": 10},
+                {"x": 0, "y": -10},
+                {"x": 60, "y": -10},
+                {"x": 60, "y": 10},
+            ],
+            "angle": 90,
+        }
+        tr_dirbend = FTL.DirBend(json_db)
+        self.process_transformation(tr_dirbend)
 
-input.append(tr_zbend)
-input.append(tr_dirbend)
-input.append(tr_lin)
-input.append(tr_spiral)
+    def test_LinearTransformation(self):
+        angle = -pi / 6
+        mat = [
+            [cos(angle), 0, sin(angle), 0],
+            [0, 1, 0, 0],
+            [-sin(angle), 0, cos(angle), 0],
+        ]
+        bounds = box(-60, -10, 60, 10)
+        tr_lin = FTL.LinearTransformation(
+            mat,
+            bounds,
+        )
+        self.process_transformation(tr_lin)
 
-process_all(input, "data/Teststrip.stl")
+    def test_Spiral(self):
+        json_sp = {
+            "points": [
+                {"x": -40, "y": 10},
+                {"x": -20, "y": -10},
+            ],
+            "length": 70,
+            "dir": "POSX",
+            "turns": 1,
+        }
+        tr_spiral = FTL.Spiral(json_sp)
+        self.process_transformation(tr_spiral)
+
+    """def test_(self):
+
+        self.process_transformation(tr_dirbend, "data/Teststrip.stl")
+    """
