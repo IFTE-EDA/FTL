b1fc7edc891e3952b4e65b452b9f488d132cfcef
Nico <nico.arnold@tu-dresden.de>
2024-07-29 15:22:18 +0200

    Add 3D Geom functions

--- FTL/core/Geometry.py
+++ FTL/core/Geometry.py
@@ -1,6 +1,8 @@
 # This file contains the geometry classes and functions for the FTL library
 
 from __future__ import annotations
+import shapely as sh
+import vedo as v
 
 
 # Base class for all geometry classes
@@ -10,18 +12,33 @@ class FTLGeom:
 
 # 2D geometry class
 class FTLGeom2D:
-    polygons: list[list[tuple(float, float)]] = []
+    polygons: list[v.Polygon] = []
+    z: float = 0
 
-    def __init__(self):
-        self.polygons = []
+    def __init__(self, z: float = 0, polygons: list[v.Polygon] = None):
+        self.polygons = polygons if polygons is not None else []
+        self.z = z
 
     def add_polygon(self, polygon: list[tuple(float, float)]):
         self.polygons.append(polygon)
 
-    def extrude(self, zstart: float, thickness: float) -> FTLGeom3D:
-        pass
+    def extrude(self, thickness: float, zpos: float = None) -> v.Mesh:
+        translate_z = self.z if zpos is None else zpos
+        if translate_z == 0:
+            return [poly.extrude(thickness) for poly in self.polygons]
+        else:
+            return [
+                poly.translate([0, 0, translate_z]).extrude(thickness)
+                for poly in self.polygons
+            ]
+
+    def to_3D(self, thickness: float, zpos: float = None) -> FTLGeom3D:
+        return FTLGeom3D(self.extrude(thickness, zpos))
 
 
 # 3D geometry class
 class FTLGeom3D:
-    pass
+    objects: list[v.Mesh] = []
+
+    def __init__(self, objects: list[v.Mesh] = []):
+        self.objects = objects
