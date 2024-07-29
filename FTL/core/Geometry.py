c83d521c062f30528937a297af00fe662b1aee23
Nico <nico.arnold@tu-dresden.de>
2024-07-29 13:56:11 +0200

    Add Geometry classes

--- FTL/core/Geometry.py
+++ FTL/core/Geometry.py
@@ -1,7 +1,27 @@
+# This file contains the geometry classes and functions for the FTL library
+
+from __future__ import annotations
+
+
+# Base class for all geometry classes
 class FTLGeom:
-    #
     pass
 
 
-class FTLMesher:
+# 2D geometry class
+class FTLGeom2D:
+    polygons: list[list[tuple(float, float)]] = []
+
+    def __init__(self):
+        self.polygons = []
+
+    def add_polygon(self, polygon: list[tuple(float, float)]):
+        self.polygons.append(polygon)
+
+    def extrude(self, zstart: float, thickness: float) -> FTLGeom3D:
+        pass
+
+
+# 3D geometry class
+class FTLGeom3D:
     pass
