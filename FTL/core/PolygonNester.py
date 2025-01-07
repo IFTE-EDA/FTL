fc7124f8d4b9de0cf9ef69cefda3c156fe5ed41a
Nico <nico.arnold@tu-dresden.de>
2025-01-07 14:32:28 +0100

    Swap polygons and holes if they are contained in a new polygon

--- FTL/core/PolygonNester.py
+++ FTL/core/PolygonNester.py
@@ -7,17 +7,52 @@ class PolygonNester:
             polygons if polygons is not None else []
         )  # np.empty(0, dtype=Polygon)
 
-    def add_polygon(self, polygon):
-        exit_loop = False
+    def dump(self):
         for poly in self.polygons:
-            print(f"Checking {polygon}")
+            print(f"| {poly}")
+            for child in poly.children:
+                print(f"|\t {child}")
+
+    def add_polygon(self, polygon):
+        def recalc():
+            for poly_recalc in poly_recalcs:
+                self.add_polygon(poly_recalc)
+
+        exit_loop = False
+        poly_recalcs = np.array([], dtype=Polygon)
+        self.dump()
+        print(f">>>Sorting {polygon}<<<")
+        for poly in self.polygons:
+            print(f"Checking {poly}")
             if not poly.overlaps(polygon):
                 print(f"\tPolygon {polygon} does not overlap {poly}")
                 continue
+            if polygon.contains(poly):
+                print(
+                    f"\t{poly} is contained in {polygon} - removing to reorder..."
+                )
+                # print(np.where(self.polygons == poly))
+                self.polygons = np.delete(
+                    self.polygons, np.isin(self.polygons, [poly])
+                )
+                poly_recalcs = np.append(poly_recalcs, poly)
+                poly_recalcs = np.append(poly_recalcs, poly.children)
+                poly.children = np.array([], dtype=Polygon)
             if poly.contains(polygon):
                 print(
                     f"\tPolygon {polygon} is contained in {poly} - checking children..."
                 )
+                if len(poly.children) > 0 and poly.children_contained_in(
+                    polygon
+                ):
+                    print(
+                        f"\t\tAll children of {poly} are contained in {polygon} - removing to reorder..."
+                    )
+                    polygon.parent = poly
+                    poly_recalcs = np.append(poly_recalcs, poly.children)
+                    poly.children = np.array([polygon], dtype=Polygon)
+                    recalc()
+                    return
                 for child in poly.children:
                     if child.contains(polygon):
                         print(
@@ -34,23 +69,10 @@ class PolygonNester:
                 )
                 poly.children = np.append(poly.children, polygon)
                 polygon.parent = poly
+                recalc()
                 return
-                """if polygon.contains(poly):
-                print(f"\tPolygon {poly} is contained in {polygon} - checking children...")
-                """
-                """for child in polygon.children:
-                    if child.contains(poly):
-                        print(f"\t\tPolygon {poly} is contained in child {child} - creating new polygon")
-                        self.polygons.append(poly)
-                        return
-                print(f"\tPolygon {poly} is not contained in a child element - adding as new child")
-                """
-                """
-                polygon.children = [poly]#np.append(polygon.children, poly)
-                poly.parent = polygon
-                return
-                """
-        self.polygons.append(polygon)
+        self.polygons = np.append(self.polygons, polygon)
+        recalc()
 
     """def nest(self):
         for poly in self.polygons:
@@ -88,7 +110,18 @@ class Polygon:
             return None
         return (np.min(self.points, axis=0), np.max(self.points, axis=0))
 
+    @property
+    def children_bounding_box(self):
+        if len(self.children) == 0:
+            return None
+        return (
+            np.min([ch.bounding_box[0] for ch in self.children], axis=0),
+            np.max([ch.bounding_box[1] for ch in self.children], axis=0),
+        )
+
     def contains(self, item):
+        if item is None:
+            return False
         bb2 = self.bounding_box
         bb1 = item.bounding_box
 
@@ -97,8 +130,34 @@ class Polygon:
         else:
             return False
 
+    def children_contained_in(self, item):
+        if item is None or len(item.points) == 0:
+            return False
+        bb1 = self.children_bounding_box
+        bb2 = item.bounding_box
+
+        if (bb1[0] >= bb2[0]).all() and (bb1[1] <= bb2[1]).all():
+            return True
+        else:
+            return False
+
     def overlaps(self, item):
+        if item is None:
+            return False
         bb2 = self.bounding_box
+        bb1 = item.bounding_box
+
+        if (bb1[0][0] < bb2[1][0]) == (bb1[1][0] > bb2[0][0]) and (
+            bb1[0][1] < bb2[1][1]
+        ) == (bb1[1][1] > bb2[0][1]):
+            return True
+        else:
+            return False
+
+    def children_overlap(self, item):
+        if item is None or len(item.points) == 0:
+            return False
+        bb2 = self.children_bounding_box
         bb1 = item.bounding_box
 
         if (bb1[0][0] < bb2[1][0]) == (bb1[1][0] > bb2[0][0]) and (
--- tests/unit/core/test_PolygonNester.py
+++ tests/unit/core/test_PolygonNester.py
@@ -46,6 +46,23 @@ class Test_PolygonNester:
         p = Polygon([(0, 0), (1, 0), (1, 0), (0, 0)])
         assert np.array_equal(p.bounding_box, [(0, 0), (1, 0)])
 
+    def test_polygon_children_bounding_box_1_rect(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array([Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])])
+        print(p.children_bounding_box)
+        assert np.array_equal(p.children_bounding_box, [(1, 1), (2, 2)])
+
+    def test_polygon_children_bounding_box_2_rects(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        print(p.children_bounding_box)
+        assert np.array_equal(p.children_bounding_box, [(1, 1), (4, 4)])
+
     """---------- BBOX COLLISION TEST ----------"""
 
     @pytest.mark.xfail
@@ -144,6 +161,80 @@ class Test_PolygonNester:
         p2 = Polygon([(-1, 1), (4, 1), (4, 2), (-1, 2)])
         assert p1.overlaps(p2)
         assert p2.overlaps(p1)
+
+    """---------- BBOX COLLISION TEST ----------"""
+
+    def test_polygon_children_overlap_empty(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon()
+        assert not p.children_overlap(p_test)
+
+    def test_polygon_children_overlap_1_rect(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon([(0.5, 0.5), (4.5, 0.5), (4.5, 4.5), (0.5, 4.5)])
+        assert p.children_overlap(p_test)
+
+    def test_polygon_children_dont_overlap_1_rect(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon([(0.5, 0.5), (0.8, 0.5), (0.8, 0.8), (0.5, 0.8)])
+        print(p.children_bounding_box)
+        print(p_test.bounding_box)
+        assert not p.children_overlap(p_test)
+
+    def test_polygon_children_contained_in_empty(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon()
+        assert not p.children_contained_in(p_test)
+
+    def test_polygon_children_contained_in_1_rect(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon([(0.5, 0.5), (4.5, 0.5), (4.5, 4.5), (0.5, 4.5)])
+        print(p.children_bounding_box)
+        print(p_test.bounding_box)
+        assert p.children_contained_in(p_test)
+
+    def test_polygon_children_not_contained_in_1_rect(self):
+        p = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+        p.children = np.array(
+            [
+                Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
+                Polygon([(3, 3), (4, 3), (4, 4), (3, 4)]),
+            ]
+        )
+        p_test = Polygon([(0.5, 0.5), (0.8, 0.5), (0.8, 0.8), (0.5, 0.8)])
+        print(p.children_bounding_box)
+        print(p_test.bounding_box)
+        assert not p.children_contained_in(p_test)
 
     """---------- NESTER TEST ----------"""
 
@@ -246,6 +337,211 @@ class Test_PolygonNester:
         assert list(p3.children) == [p4]
         assert p3.parent is None
         assert p4.parent == p3
+
+    def test_polygon_nester_nest_square_containing_2_squares_kissing_corners(
+        self,
+    ):
+        Polygon.reset()
+        p1 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p2 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p3 = Polygon([(2, 2), (2.5, 2), (2.5, 2.5), (2, 2.5)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1]
+        assert list(p1.children) == [p2, p3]
+        assert p2.parent == p1
+        assert p3.parent == p1
+
+    def test_polygon_nester_nest_square_containing_2_squares_kissing_edges(
+        self,
+    ):
+        Polygon.reset()
+        p1 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p2 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p3 = Polygon([(2, 1), (2.5, 1), (2.5, 2), (2, 2)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1]
+        assert list(p1.children) == [p2, p3]
+        assert p2.parent == p1
+        assert p3.parent == p1
+
+    def test_polygon_nester_nest_square_containing_2_overlapping_squares(self):
+        Polygon.reset()
+        p1 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p2 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p3 = Polygon([(1.5, 1.5), (2.5, 1.5), (2.5, 2.5), (1.5, 2.5)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1]
+        assert list(p1.children) == [p2, p3]
+        assert p2.parent == p1
+        assert p3.parent == p1
+
+    def test_polygon_nester_nest_4_overlapping_squares(self):
+        Polygon.reset()
+        p1 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p2 = Polygon([(1, 1), (4, 1), (4, 4), (1, 4)])
+        p3 = Polygon([(2, 2), (5, 2), (5, 5), (2, 5)])
+        p4 = Polygon([(3, 3), (6, 3), (6, 6), (3, 6)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        print("P3: ", p3)
+        print("P4: ", p4)
+        pn.add_polygon(p1)
+        pn.add_polygon(p2)
+        pn.add_polygon(p3)
+        pn.add_polygon(p4)
+        assert list(pn.polygons) == [p1, p2, p3, p4]
+        assert list(p1.children) == []
+        assert list(p2.children) == []
+        assert list(p3.children) == []
+        assert list(p4.children) == []
+
+    def test_polygon_nester_nest_3_overlapping_squares_enclosed_in_new_square(
+        self,
+    ):
+        Polygon.reset()
+        p1 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p2 = Polygon([(1.25, 1.25), (1.75, 1.25), (1.75, 1.75), (1.25, 1.75)])
+        p3 = Polygon([(1.3, 1.3), (1.7, 1.3), (1.7, 1.7), (1.3, 1.7)])
+        p4 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        print("P3: ", p3)
+        print("P4: ", p4)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        print("-------------------")
+
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        print("-------------------")
+
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1, p3]
+        assert list(p1.children) == [p2]
+        assert p2.parent == p1
+        assert p3.parent is None
+        print("-------------------")
+
+        pn.add_polygon(p4)
+        assert list(pn.polygons) == [p4, p2]
+        assert list(p4.children) == [p1]
+        assert p1.parent == p4
+        assert list(p2.children) == [p3]
+        assert p3.parent == p2
+        assert list(p3.children) == []
+        assert p4.parent is None
+
+    def test_polygon_nester_nest_3_overlapping_squares_enclosed_in_2_new_squares(
+        self,
+    ):
+        Polygon.reset()
+        p1 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p2 = Polygon([(1.25, 1.25), (1.75, 1.25), (1.75, 1.75), (1.25, 1.75)])
+        p3 = Polygon([(1.3, 1.3), (1.7, 1.3), (1.7, 1.7), (1.3, 1.7)])
+        p4 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p5 = Polygon([(-1, -1), (4, -1), (4, 4), (-1, 4)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        print("P3: ", p3)
+        print("P4: ", p4)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        print("-------------------")
+
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        print("-------------------")
+
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1, p3]
+        assert list(p1.children) == [p2]
+        assert p2.parent == p1
+        assert p3.parent is None
+        print("-------------------")
+
+        pn.add_polygon(p4)
+        assert list(pn.polygons) == [p4, p2]
+        assert list(p4.children) == [p1]
+        assert p1.parent == p4
+        assert list(p2.children) == [p3]
+        assert p3.parent == p2
+        assert list(p3.children) == []
+        assert p4.parent is None
+        print("-------------------")
+
+        pn.add_polygon(p5)
+        pn.dump()
+        assert list(pn.polygons) == [p5, p1, p3]
+
+    def test_polygon_nester_add_rect_between_rect_and_children(self):
+        Polygon.reset()
+        p1 = Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        p2 = Polygon([(1, 1), (2, 1), (2, 2), (1, 2)])
+        p3 = Polygon([(1.25, 1.25), (1.75, 1.25), (1.75, 1.75), (1.25, 1.75)])
+        p4 = Polygon([(0.5, 0.5), (2.5, 0.5), (2.5, 2.5), (0.5, 2.5)])
+        pn = PolygonNester()
+        print("P1: ", p1)
+        print("P2: ", p2)
+        print("P3: ", p3)
+        print("P4: ", p4)
+        pn.add_polygon(p1)
+        assert pn.polygons == [p1]
+        print("-------------------")
+
+        pn.add_polygon(p2)
+        assert pn.polygons == [p1]
+        assert p1.children == [p2]
+        assert p2.parent == p1
+        print("-------------------")
+
+        pn.add_polygon(p3)
+        assert list(pn.polygons) == [p1, p3]
+        assert list(p1.children) == [p2]
+        assert p2.parent == p1
+        assert p3.parent is None
+        print("-------------------")
+
+        pn.add_polygon(p4)
+        pn.dump()
+        assert list(pn.polygons) == [p1, p2]
+        assert list(p1.children) == [p4]
+        assert p4.parent == p1
+        assert list(p2.children) == [p3]
+        assert p3.parent == p2
 
     """
     def test_polygon_nester_nest_square_containing_2_squares(self):
