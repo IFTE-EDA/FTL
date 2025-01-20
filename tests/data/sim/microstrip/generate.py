df09994ce052ae4c80865f5ab4df458acc5f1767
Nico <nico.arnold@tu-dresden.de>
2025-01-20 16:41:05 +0100

    Add model creator for Microstrip sim usecase test

--- tests/data/sim/microstrip/generate.py
+++ tests/data/sim/microstrip/generate.py
@@ -8,6 +8,9 @@ sub_w = 10000
 trace_w = 150
 trace_l = 50000
 
+thick_metal = 100
+thick_sub = 1400
+
 
 def main():
     doc = ezdxf.new("R2010")
@@ -16,6 +19,8 @@ def main():
     doc.layers.add(name="sub")
     doc.layers.add(name="top_metal")
     doc.layers.add(name="bot_metal")
+    doc.layers.add(name="port_1")
+    doc.layers.add(name="port_2")
 
     polyline_trace = [
         (-trace_l / 2, -trace_w / 2),
@@ -40,6 +45,19 @@ def main():
     gnd = msp.add_lwpolyline(polyline_sub, dxfattribs={"layer": "bot_metal"})
     gnd.close(True)
     gnd.dxf.const_width = 0
+
+    port_1 = msp.add_line(
+        (-sub_l / 2, -trace_w / 2),
+        (-sub_l / 2, trace_w / 2),
+        dxfattribs={"layer": "port_1"},
+    )
+    port_1.dxf.lineweight = 0
+    port_2 = msp.add_line(
+        (sub_l / 2, -trace_w / 2),
+        (sub_l / 2, trace_w / 2),
+        dxfattribs={"layer": "port_2"},
+    )
+    port_2.dxf.lineweight = 0
 
     filename = data_dir.name + ".dxf"
     doc.saveas(data_dir / filename)
