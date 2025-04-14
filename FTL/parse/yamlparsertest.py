f3d605239fd63438b290c84c0bf8f69dd689515e
Nico <nico.arnold@tu-dresden.de>
2025-04-14 16:52:07 +0200

    Test file for YAML parsing

---
+++ FTL/parse/yamlparsertest.py
@@ -0,0 +1,13 @@
+import yaml
+import gmsh
+from YAMLParser import YAMLParser
+
+# with open("stackup_test.yaml", "r") as f:
+#    data = yaml.safe_load(f)
+# print(data)
+
+parser = YAMLParser("stackup_test.yaml")
+parser.make_geometry()
+
+gmsh.model.occ.synchronize()
+gmsh.fltk.run()
